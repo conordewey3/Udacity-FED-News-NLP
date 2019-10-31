eece5016d40e35c53bf2732f30f01aabdd3f19da
conordewey3 <cdewey96@vt.edu>
2019-10-30 21:52:50 -0400

    setting up aylien

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var aylien = require("aylien_textapi");
+const dotenv = require('dotenv');
+dotenv.config();
 
+// API credentials
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+  
 const app = express()
 
 app.use(express.static('dist'))
